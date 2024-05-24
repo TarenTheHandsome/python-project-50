d241709a3e34ffc671682a02782fbe177992d026
Elena <ziggy.taren.44@gmail.com>
2024-05-24 17:16:59 +0300

    the first step of the project

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import argparse
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    args = parser.parse_args()
+
+
+
+if __name__ == '__main__':
+    main()
