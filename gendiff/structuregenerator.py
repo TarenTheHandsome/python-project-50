9ad3eb024a20923440a22aa4237b669f4d908b33
Elena <ziggy.taren.44@gmail.com>
2025-10-25 11:56:50 +0300

    'structure without "value_form"'

--- gendiff/structuregenerator.py
+++ gendiff/structuregenerator.py
@@ -1,90 +1,85 @@
 import copy
 
-def find_key_status(key, old, new):
-    if key not in old and key in new:
-        return 'added'
-    elif key in old and key not in new:
-        return 'removed'
-    elif key in old and key in new:
-        if old[key] != new[key]:
+
+def get_nested_status(value):
+    for i in value:
+        if isinstance(i, dict):
+            return True
+    return False
+
+
+def get_simple_status(value):
+    if len(value) == 4:
+        if value[1] == value[3]:
+            return 'unchanged'
+        else:
+            if isinstance(value[1], dict) and not isinstance(value[3], dict):
+                return 'old_nest'
+            elif not isinstance(value[1], dict) and isinstance(value[3], dict):
+                return 'new_nest'
             return 'changed'
-        return 'unchanged'
-
-
-def find_value_form(key, old, new):
-    if (key in old and isinstance(old[key], dict)) \
-            or (key in new and isinstance(new[key], dict)):
-        return 'nested'
-    return 'simple'
-
-
-def check_value(value):
-    if len(value) == 1:
-        return value[0]
     else:
-        if value[0] == value[1]:
-            return value[0]
-        return value
+        if 'old' in value:
+            return 'removed'
+        elif 'new' in value:
+            return 'added'
 
 
-def get_value(key, old, new):
-    value = []
-    if key in old:
-        value.append(old[key])
-    if key in new:
-        value.append(new[key])
-
-    return check_value(value)
+def get_status(value):
+    if get_nested_status(value):
+        return 'nested'
+    else:
+        return get_simple_status(value)
 
 
-def structure_generator(old={}, new={}):
-    if not isinstance(old, dict):
-        old = {}
-    if not isinstance(new, dict):
-        new = {}
+def build_diff(old={}, new={}):
     structure = []
-    workpiece_original = {'value_form': 'simple', 'key_status': 'unchanged',
-                  'key': '', 'value': ''}
 
     all_keys = sorted(set(list(old.keys()) + list(new.keys())))
+
+    workpiece_original = {'key_status': '', 'key': ''}
 
     for key in all_keys:
         workpiece = copy.deepcopy(workpiece_original)
         workpiece['key'] = key
-        workpiece['value_form'] = find_value_form(key, old, new)
-        workpiece['key_status'] = find_key_status(key, old, new)
-        value = get_value(key, old, new)
+        value = []
 
-        if workpiece['value_form'] == 'simple':
-            if workpiece['key_status'] == 'changed':
-                workpiece['value'] = {'old_value': value[0], 'new_value': value[1]}
-            else:
-                workpiece['value'] = value
+        if key in old:
+            value.append('old')
+            value.append(old[key])
+        if key in new:
+            value.append('new')
+            value.append(new[key])
 
-        elif workpiece['value_form'] == 'nested':
-            if workpiece['key_status'] == 'changed':
-                if isinstance(value[0], dict) and isinstance(value[1], dict):
-                    workpiece['value'] = structure_generator(value[0], value[1])
+        workpiece['key_status'] = get_status(value)
 
-                elif not isinstance(value[0], dict) and isinstance(value[1], dict):
-                    old_value = value[0]
-                    new_value = structure_generator({}, value[1])
-                    workpiece['value'] = {'old_value': old_value, 'new_value': new_value}
+        if workpiece['key_status'] == 'nested':
+            status = get_simple_status(value)
 
-                elif isinstance(value[0], dict) and not isinstance(value[1], dict):
-                    old_value = structure_generator({}, value[0])
-                    new_value = value[1]
-                    workpiece['value'] = {'old_value': old_value, 'new_value': new_value}
+            if status == 'added' or status == 'added' or status == 'unchanged':
+                workpiece['children'] = build_diff(value[1], {})
 
+            elif status == 'changed':
+                workpiece['children'] = build_diff(value[1], value[3])
 
-            else:
-                workpiece['value'] = structure_generator(value)
+            elif status == 'old_nest':
+                workpiece['children'] = {'old_value': build_diff(value[1], {}), 'new_value': value[3]}
 
+            elif status == 'new_nest':
+                workpiece['children'] = {'old_value': value[1], 'new_value': build_diff(value[3], {})}
+
+        elif workpiece['key_status'] == 'added' or workpiece['key_status'] == 'removed' \
+                or workpiece['key_status'] == 'unchanged':
+            workpiece['value'] = value[1]
+
+        elif workpiece['key_status'] == 'changed':
+            workpiece['old_value'] = value[1]
+            workpiece['new_value'] = value[3]
 
         structure.append(workpiece)
 
-
     return structure
+
 
 
 
