ad271240f450f09a2c6cd3b429a75273173063de
Elena <ziggy.taren.44@gmail.com>
2025-11-07 12:21:33 +0300

    'new stylish'

--- gendiff/structuregenerator.py
+++ gendiff/structuregenerator.py
@@ -4,64 +4,51 @@ import copy
 def build_diff(old={}, new={}):
     structure = []
 
-    all_keys = sorted(set(list(old.keys()) + list(new.keys())))
-
-    workpiece_original = {'key_status': '', 'key': ''}
+    all_keys = sorted(old.keys() | new.keys())
 
     for key in all_keys:
-        workpiece = copy.deepcopy(workpiece_original)
-        workpiece['key'] = key
+        workpiece = {'key': key}
+
 
         if key not in old:
-            value = new[key]
+            workpiece['key_status'] = 'added'
+            workpiece['value'] = new[key]
 
-            if isinstance(value, dict):
-                workpiece['key_status'] = 'nested'
-                workpiece['children'] = build_diff(value, {})
-            else:
-                workpiece['key_status'] = 'added'
-                workpiece['value'] = value
+        elif key not in new:
+            workpiece['key_status'] = 'removed'
+            workpiece['value'] = old[key]
 
-        if key not in new:
-            value = old[key]
-
-            if isinstance(value, dict):
-                workpiece['key_status'] = 'nested'
-                workpiece['children'] = build_diff(value, {})
-            else:
-                workpiece['key_status'] = 'removed'
-                workpiece['value'] = value
-
-        if key in old and key in new:
+        else:
             old_value = old[key]
             new_value = new[key]
 
-            if old_value == new_value:
-                if isinstance(old_value, dict):
-                    workpiece['key_status'] = 'nested'
-                    workpiece['children'] = build_diff(old_value, {})
-                else:
-                    workpiece['key_status'] = 'unchanged'
-                    workpiece['value'] = old_value
+            if isinstance(old_value, dict) and isinstance(new_value, dict):
+                workpiece['key_status'] = 'nested'
+                workpiece['children'] = build_diff(old_value, new_value)
+
+            elif old_value == new_value:
+                workpiece['key_status'] = 'unchanged'
+                workpiece['value'] = old_value
 
             else:
-                if isinstance(old_value, dict):
-                    workpiece['key_status'] = 'nested'
-                    if isinstance(new_value, dict):
-                        workpiece['children'] = build_diff(old_value, new_value)
+                workpiece['key_status'] = 'changed'
+                workpiece['old_value'] = old_value
+                workpiece['new_value'] = new_value
 
-                    else:
-                        workpiece['children'] = {'old_value': build_diff(old_value, {}), 'new_value': new_value}
-
-                elif isinstance(new_value, dict) and not isinstance(old_value, dict):
-                    workpiece['key_status'] = 'nested'
-                    workpiece['children'] = {'old_value': old_value, 'new_value': build_diff(new_value, {})}
-
-                elif not isinstance(old_value, dict) and not isinstance(new_value, dict):
-                    workpiece['key_status'] = 'changed'
-                    workpiece['old_value'] = old_value
-                    workpiece['new_value'] = new_value
 
         structure.append(workpiece)
 
     return structure
+
+
+
+flat1 = {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}
+flat2 = {'timeout': 20, 'verbose': True, 'host': 'hexlet.io'}
+
+print(build_diff(flat1, flat2))
+
+new1 = {'common': {'setting1': 'Value 1', 'setting2': 200, 'setting3': True, 'setting6': {'key': 'value', 'doge': {'wow': 'value'}}}, 'group1': {'baz': 'bas', 'foo': 'bar', 'nest': {'key': 'value'}}, 'group2': {'abc': 12345, 'deep': {'id': 45}}}
+new2 = {'common': {'follow': False, 'setting1': 'Value 1', 'setting3': None, 'setting4': 'blah blah', 'setting5': {'key5': 'value5'}, 'setting6': {'key': 'value', 'ops': 'vops', 'doge': {'wow': 'so much'}}}, 'group1': {'foo': 'bar', 'baz': 'bars', 'nest': 'str'}, 'group3': {'deep': {'id': {'number': 45}}, 'fee': 100500}}
+
+print(build_diff(new1, new2))
+
--- gendiff/stylishresult.py
+++ gendiff/stylishresult.py
@@ -1,50 +1,66 @@
+def flatten_gen(nested, isatom=lambda x: not isinstance(x, list)):
+    for item in nested:
+        if isatom(item):
+            yield item
+        else:
+            yield from flatten_gen(item)
 
-def make_string(key_status, key, value):
-    if isinstance(value, dict):
-        return f"{key_status} {key}:""{"
-    if isinstance(value, list):
-        return f"{key_status} {key}:""{"
-    else:
-        return f"{key_status} {key}:{value}"
 
-def stylish_formater(structure):
-    #результирующий список
+def jack_the_pipper(data, counter):
+    level = '*' * counter
     lines = []
+    for key in data:
+        value = data[key]
+        if isinstance(value, dict):
+            lines.append([f'{level} {key}: {"{"}', jack_the_pipper(value, counter + 1)])
 
-
-    status = {'changed': ' ' , 'unchanged': ' ', 'removed': '-', 'added': '+'}
-
-    for line in structure:
-        key = line['key']
-        value = line['value']
-        key_status = line['key_status']
-        value_form = line['value_form']
-
-        if value_form == 'simple':
-            if line['key_status'] == 'changed':
-                lines.append(make_string(status['removed'], key, value['old_value']))
-                lines.append(make_string(status['added'], key, value['new_value']))
-            else:
-                lines.append(make_string(status[key_status], key, value))
-
-        if value_form == 'nested':
-            if line['key_status'] == 'changed':
-                if isinstance(value, list):
-                    lines.append(stylish_formater(value))
-                elif isinstance(value, dict):
-                    if isinstance(value['old_value'], list):
-                        lines.append(stylish_formater(value['old_value']))
-                    else:
-                        lines.append(make_string(status['removed'], key, value['old_value']))
-                    if isinstance(value['new_value'], list):
-                        lines.append(stylish_formater(value['new_value']))
-                    else:
-                        lines.append(make_string(status['added'], key, value['new_value']))
-            else:
-                 lines.append(stylish_formater(value))
-
-        lines.insert(-1, make_string(status[key_status], key, value))
-
+        else:
+            lines.append(f'{level} {key}: {value}')
 
     return lines
 
+
+def stylish_formater(structure, counter=1):
+    lines = []
+    status = {'changed': ' ', 'unchanged': ' ', 'removed': '-', 'added': '+', 'nested': '?'}
+    level = '*' * counter
+
+    for line in structure:
+
+        key = line['key']
+        key_status = line['key_status']
+
+        if key_status == 'nested':
+            lines.append([f'{level} {status[key_status]} {key}: {"{"}',
+                          stylish_formater(line['children'], counter + 1)])
+
+        elif key_status == 'changed':
+            if isinstance(line['old_value'], dict):
+                lines.append([f"{level} {status['removed']} {key}: {'{'}",
+                              jack_the_pipper(line['old_value'], counter + 1)])
+            else:
+                lines.append(f"{level} {status['removed']} {key}: {line['old_value']}")
+
+            if isinstance(line['new_value'], dict):
+                lines.append([f"{level} {status['added']} {key}: {'{'}",
+                              jack_the_pipper(line['new_value'], counter + 1)])
+            else:
+                lines.append(f"{level} {status['added']} {key}: {line['new_value']}")
+
+
+        elif key_status in ['unchanged', 'removed', 'added']:
+            if isinstance(line['value'], dict):
+                lines.append([f"{level} {status[key_status]} {key}: {'{'}",
+                              jack_the_pipper(line['value'], counter + 1)])
+            else:
+                lines.append(f"{level} {status[key_status]} {key}: {line['value']}")
+
+    return lines
+
+
+
+flat = [{'key': 'follow', 'key_status': 'removed', 'value': False}, {'key': 'host', 'key_status': 'unchanged', 'value': 'hexlet.io'}, {'key': 'proxy', 'key_status': 'removed', 'value': '123.234.53.22'}, {'key': 'timeout', 'key_status': 'changed', 'old_value': 50, 'new_value': 20}, {'key': 'verbose', 'key_status': 'added', 'value': True}]
+nested = [{'key': 'common', 'key_status': 'nested', 'children': [{'key': 'follow', 'key_status': 'added', 'value': False}, {'key': 'setting1', 'key_status': 'unchanged', 'value': 'Value 1'}, {'key': 'setting2', 'key_status': 'removed', 'value': 200}, {'key': 'setting3', 'key_status': 'changed', 'old_value': True, 'new_value': None}, {'key': 'setting4', 'key_status': 'added', 'value': 'blah blah'}, {'key': 'setting5', 'key_status': 'added', 'value': {'key5': 'value5'}}, {'key': 'setting6', 'key_status': 'nested', 'children': [{'key': 'doge', 'key_status': 'nested', 'children': [{'key': 'wow', 'key_status': 'changed', 'old_value': 'value', 'new_value': 'so much'}]}, {'key': 'key', 'key_status': 'unchanged', 'value': 'value'}, {'key': 'ops', 'key_status': 'added', 'value': 'vops'}]}]}, {'key': 'group1', 'key_status': 'nested', 'children': [{'key': 'baz', 'key_status': 'changed', 'old_value': 'bas', 'new_value': 'bars'}, {'key': 'foo', 'key_status': 'unchanged', 'value': 'bar'}, {'key': 'nest', 'key_status': 'changed', 'old_value': {'key': 'value'}, 'new_value': 'str'}]}, {'key': 'group2', 'key_status': 'removed', 'value': {'abc': 12345, 'deep': {'id': 45}}}, {'key': 'group3', 'key_status': 'added', 'value': {'deep': {'id': {'number': 45}}, 'fee': 100500}}]
+
+print('\n'.join(flatten_gen(stylish_formater(flat))))
+print('\n'.join(flatten_gen(stylish_formater(nested))))
